e81b8f3f77d3646641aaa4cb4033bd57cd1b0ef7
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-11-01 12:37:03 -0400

    maybefix

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ernest
+ * Created on: Oct 2023
+ * This program counts down
 */
 
-basic.showString('Hello, World!')
+let loopCount: number = 4
+
+// setup 
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// on button press
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  let neoPixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neoPixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neoPixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+  while (loopCount < -1) {
+    basic.showString(loopCount.toString())
+    neoPixelStrip.setPixelColor(loopCount, neopixel.colors(NeoPixelColors.Black))
+    loopCount = loopCount - 1
+
+  if (loopCount = -1)
+    basic.showIcon(IconNames.Sad)
+    basic.pause(1000)
+    basic.clearScreen()
+  }
+})
