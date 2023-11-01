98d20c24b9eed57829001972ad020c6a00b00ea0
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-11-01 13:27:44 -0400

    typever

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@
  * This program counts down
 */
 
+// variables
 let loopCount: number = 4
 
 // setup 
@@ -14,19 +15,19 @@ basic.showIcon(IconNames.Happy)
 // on button press
 input.onButtonPressed(Button.A, function () {
   basic.clearScreen()
-  let neoPixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-    neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-    neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
-    neoPixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-    neoPixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
-  while (loopCount < -1) {
+  const neoPixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+  neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+  neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+  neoPixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+  neoPixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+  while (loopCount > -1) {
+    // countdown
     basic.showString(loopCount.toString())
     neoPixelStrip.setPixelColor(loopCount, neopixel.colors(NeoPixelColors.Black))
     loopCount = loopCount - 1
-
-  if (loopCount = -1)
-    basic.showIcon(IconNames.Sad)
-    basic.pause(1000)
-    basic.clearScreen()
-  }
-})
+    // change loopCount
+    if (loopCount <= -1) {
+      basic.showIcon(IconNames.Sad)
+      basic.pause(1000)
+      basic.clearScreen()
+  })
